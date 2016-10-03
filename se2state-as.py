445feee28213fc01959a5f8751596aef9063874e
Andreas Schrell <as@schrell.de>
2016-10-03 18:57:56 +0000

    se2state behält letzten Ertragswert des Tages und Uptime des Wechselrichters

--- se2state-as.py
+++ se2state-as.py
@@ -26,8 +26,8 @@ except:
         
 
 # initialize empty reminder dictionary
-lastDict = {"optimizers": {}}
-aggregatedDict = {"optimizers": {}}
+lastDict = {"optimizers": {}, "inverters": {}}
+aggregatedDict = {"optimizers": {}, "inverters" : {}}
 # read the input forever
 while True:
     jsonStr = ""
@@ -47,17 +47,32 @@ while True:
                     else:
                         aggregatedDict["optimizers"][optimizer] = lastDict["optimizers"][optimizer].copy()
     	lastDict["optimizers"][optimizer] = inDict["optimizers"][optimizer].copy()
+    # check for resetted inverters and handle uptime an eday for them
+    for inverter in inDict["inverters"].keys():
+        if inverter in lastDict["inverters"]:
+            if inDict["inverters"][inverter]["Date"] == stateDict["inverters"][inverter]["Date"]:
+                if int(inDict["inverters"][inverter]["Uptime"]) < int(stateDict["inverters"][inverter]["Uptime"]):
+                    if inverter in aggregatedDict["inverters"].keys():
+                        aggregatedDict["inverters"][inverter]["Uptime"] += lastDict["inverters"][inverter]["Uptime"]
+                        aggregatedDict["inverters"][inverter]["Eday"] += lastDict["inverters"][inverter]["Eday"]
+                    else:
+                        aggregatedDict["inverters"][inverter] = lastDict["inverters"][inverter].copy()
+        lastDict["inverters"][inverter] = inDict["inverters"][inverter].copy()
     # update the state values
     stateDict["inverters"].update(inDict["inverters"])
     stateDict["optimizers"].update(inDict["optimizers"])
-
     # update the state file width the current and aggregated values
     with open(outFileName, "w") as outFile:
         tempDict = {"inverters": {}, "optimizers": {}}
-        tempDict["inverters"] = stateDict["inverters"]
         for optimizer in stateDict["optimizers"].keys():
             tempDict["optimizers"][optimizer] = stateDict["optimizers"][optimizer].copy();
             if optimizer in aggregatedDict["optimizers"].keys():
                 tempDict["optimizers"][optimizer]["Uptime"] += aggregatedDict["optimizers"][optimizer]["Uptime"]
 	        tempDict["optimizers"][optimizer]["Eday"] += aggregatedDict["optimizers"][optimizer]["Eday"]
+        for inverter in stateDict["inverters"].keys():
+            tempDict["inverters"][inverter] = stateDict["inverters"][inverter].copy();
+            if inverter in aggregatedDict["inverters"].keys():
+                tempDict["inverters"][inverter]["Uptime"] += aggregatedDict["inverters"][inverter]["Uptime"]
+	        tempDict["inverters"][inverter]["Eday"] += aggregatedDict["inverters"][inverter]["Eday"]
         json.dump(tempDict, outFile)
+
